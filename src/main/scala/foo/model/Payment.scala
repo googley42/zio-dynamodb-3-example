c804a5cd93a0370f337f22bdaba1c36cab3998a9
Avinder Bahra <avinder.bahra@gmail.com>
2024-04-09 07:46:12 +0100

    enum

--- src/main/scala/foo/model/Payment.scala
+++ src/main/scala/foo/model/Payment.scala
@@ -3,14 +3,10 @@ package foo.model
 import zio.schema.Schema
 import zio.schema.DeriveSchema
 
-sealed trait Payment
-
+enum Payment:
+  case DebitCard
+  case CreditCard
+  case PayPal
 object Payment:
-  case object DebitCard extends Payment
-
-  case object CreditCard extends Payment
-
-  case object PayPal extends Payment
-
   implicit val schema: Schema.Enum3[DebitCard.type, CreditCard.type, PayPal.type, Payment] = DeriveSchema.gen[Payment]
 
