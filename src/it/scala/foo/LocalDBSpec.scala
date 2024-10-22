38d3a95dbba485bb3bb2e79888d28f42e6511530
Avinder Bahra <avinder.bahra@gmail.com>
2024-10-22 06:49:27 +0100

    investigate sealed trait

--- src/it/scala/foo/LocalDBSpec.scala
+++ src/it/scala/foo/LocalDBSpec.scala
@@ -6,6 +6,10 @@ import zio.dynamodb.DynamoDBQuery
 import zio.schema.DeriveSchema
 import zio.schema.Schema
 import zio.dynamodb.ProjectionExpression
+import zio.dynamodb.PrimaryKey
+import zio.schema.annotation.caseName
+import zio.schema.annotation.discriminatorName
+import zio.test.TestAspect
 
 object LocalDBSpec extends DynamoDBLocalSpec {
   def spec = suite("DynamoDB local suite")(
@@ -26,6 +30,35 @@ object LocalDBSpec extends DynamoDBLocalSpec {
             .absolve
         } yield assertTrue(x == User("1", "John"))
       )
+    },
+    test("sealed trait") {
+      @discriminatorName("animalType")
+      sealed trait Animal
+      object Animal {
+        // compiler macro error "Deriving schema for Animal is not supported"
+//        implicit val schema: Schema[Animal] = DeriveSchema.gen[Animal]
+      }
+      @caseName("the_dog")
+      final case class Dog(id: String, name: String) extends Animal
+      object Dog {
+        implicit val schema: Schema.CaseClass2[String, String, Dog] =
+          DeriveSchema.gen[Dog]
+        val (id, name) = ProjectionExpression.accessors[Dog]
+      }
+      final case class Cat(id: String, name: String) extends Animal
+      object Cat {
+        implicit val schema: Schema.CaseClass2[String, String, Cat] =
+          DeriveSchema.gen[Cat]
+        val (id, name) = ProjectionExpression.accessors[Cat]
+      }
+
+      withSingleIdKeyTable(tableName =>
+        for {
+          _ <- DynamoDBQuery.put(tableName, Dog("1", "John")).execute
+          x <- DynamoDBQuery.getItem(tableName, PrimaryKey("id" -> "1")).execute
+          _ = println(s"XXXXXXXXXXXX $x")
+        } yield assertTrue(1 == 1)
+      )
     }
-  )
+  ) @@ TestAspect.nondeterministic
 }
